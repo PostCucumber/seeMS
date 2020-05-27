13b73324269028dfdf25c811cbb2c02fa65e6ab5
postCucumber <johnston94@gmail.com>
2020-05-26 22:19:12 -0500

    add component

---
+++ resources/js/components/Columns.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-wrap w-full">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+        },
+        mounted() {
+            console.log('Columns component mounted.')
+        },
+    }
+</script>
