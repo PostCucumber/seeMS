a2a199b3008886d1f9386d2f2f0297d14841e0a9
postCucumber <johnston94@gmail.com>
2020-05-26 21:33:35 -0500

    display pages

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 @section('content')
+    @foreach($pages as $page)
+        <div>
+            <h2>{{ $page->title }}</h2>
+            <p>{{ $page->content }}</p>
+        </div>    
+    @endforeach
 @endsection
