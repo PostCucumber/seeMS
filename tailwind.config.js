92033a9e75663c806377a88fa656569db85931b7
postCucumber <johnston94@gmail.com>
2020-05-30 14:07:53 -0500

    add breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1792px',
+      // => @media (min-width: 1792px) { ... } 
+    },
     extend: {}
   },
   variants: {},
