62c46b3b60c37e68f0d982fef3716ea9f48ee721
postCucumber <johnston94@gmail.com>
2020-06-01 23:07:22 -0500

    wip

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
